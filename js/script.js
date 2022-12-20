6b6b6086e12b7e484320afc8031c98aef24f14a0
dunzolo <rossidavide108@gmail.com>
2022-12-20 14:30:56 +0100

    milestone 1 done: added todo list and line-through

--- js/script.js
+++ js/script.js
@@ -5,7 +5,36 @@ const{
 createApp({
     data() {
         return {
-            
+            todo_list:[
+                {
+                    text: 'Fare la spesa',
+                    done: true,
+                },
+                {
+                    text: 'Studiare',
+                    done: false,
+                },
+                {
+                    text: 'Comprare un nuovo telefono',
+                    done: true,
+                },
+                {
+                    text: 'Fare l\'esercizio di Boolean',
+                    done: false,
+                },
+                {
+                    text: 'Lavare la macchina',
+                    done: false,
+                },
+                {
+                    text: 'Preparare da mangiare',
+                    done: true,
+                },
+                {
+                    text: 'Fare una passeggiata',
+                    done: false,
+                }
+            ]
         }
     },
 }).mount('#app')
